6eaed5db2100749e8360b56e541ff0851134fe24
JacNo <jcknowa@gmail.com>
2021-10-16 15:44:19 +0200

    Functionality: adding meme in form, list all memes in list

--- src/main/java/pl/sdacademy/projekt3/controllers/MemeController.java
+++ src/main/java/pl/sdacademy/projekt3/controllers/MemeController.java
@@ -1,9 +1,39 @@
 package pl.sdacademy.projekt3.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.sdacademy.projekt3.entities.Meme;
+import pl.sdacademy.projekt3.repositories.MemeRepository;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/meme")
 public class MemeController {
+    private final MemeRepository memeRepository;
+
+    public MemeController(MemeRepository memeRepository) {
+        this.memeRepository = memeRepository;
+    }
+
+    @GetMapping("/list")
+    public String getMemes(ModelMap modelMap){
+        List<Meme> memes = memeRepository.findAll();
+        modelMap.addAttribute("memes",memes);
+        return "meme/memelist";
+    }
+
+    @GetMapping("/add")
+    public String addMeme(@ModelAttribute("meme") Meme meme){
+        return "meme/memeaddform";
+    }
+    @PostMapping("/add")
+    public String saveMeme(Meme meme){
+        memeRepository.save(meme);
+        return "redirect:/meme/list";
+    }
 }
--- src/main/java/pl/sdacademy/projekt3/entities/Meme.java
+++ src/main/java/pl/sdacademy/projekt3/entities/Meme.java
@@ -17,6 +17,49 @@ public class Meme {
     private String user;
     //Później obrazek
     private String description;
-    private int rating;
+    private int rating = 0;
 
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
 }
