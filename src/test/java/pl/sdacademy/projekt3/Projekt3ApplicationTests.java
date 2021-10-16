62e6f569722d7a8d0517493b6847628eb204fc2a
JacNo <jcknowa@gmail.com>
2021-10-16 13:56:57 +0200

    Initial commit

---
+++ src/test/java/pl/sdacademy/projekt3/Projekt3ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sdacademy.projekt3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projekt3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
