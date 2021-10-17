64eb066f07d451753df615a20b0f60263a5c0bf3
KarolinaMarek <karolinamarek7@wp.pl>
2021-10-17 09:31:35 +0200

    Option to add new category

--- src/main/java/pl/sdacademy/projekt3/entities/Category.java
+++ src/main/java/pl/sdacademy/projekt3/entities/Category.java
@@ -19,4 +19,12 @@ public class Category {
     public String getName() {
         return name;
     }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
