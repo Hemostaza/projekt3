ccbc223cce7a6b31abd8ee5c72d3420953e696ca
Justyna <justinere61@gmail.com>
2021-10-17 14:28:10 +0200

    Usuwanie i edycja użytkownika

--- src/main/java/pl/sdacademy/projekt3/controllers/UserController.java
+++ src/main/java/pl/sdacademy/projekt3/controllers/UserController.java
@@ -2,10 +2,7 @@ package pl.sdacademy.projekt3.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import pl.sdacademy.projekt3.entities.Meme;
 import pl.sdacademy.projekt3.entities.User;
 import pl.sdacademy.projekt3.repositories.UserRepository;
@@ -27,12 +24,31 @@ public class UserController {
         modelMap.addAttribute("users", users);
         return "user/user_list";
     }
+
     @GetMapping("/register")
-    public String registerUserForm(@ModelAttribute("user") User user){
+    public String registerUserForm(@ModelAttribute("user") User user) {
         return "user/user_register";
     }
+
     @PostMapping("/register")
-    public String registerUser(User user){
+    public String registerUser(User user) {
+        userRepository.save(user);
+        return "redirect:/user/list";
+    }
+
+    @GetMapping("/delete/by-id/{id}")
+    public String deleteUser(@PathVariable Integer id){
+        userRepository.deleteById(id);
+        return "redirect:/user/list";
+    }
+
+    @GetMapping("/edit/by-id/{id}")
+    public String editUser(@PathVariable Integer id, ModelMap modelMap){
+        modelMap.addAttribute("user",userRepository.findById(id));
+        return "user/user_edit";
+    }
+    @PostMapping("/edit")
+    public String saveEditUser(User user){
         userRepository.save(user);
         return "redirect:/user/list";
     }
