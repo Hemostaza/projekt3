709fcb6776c660546ebf32c1e9e68aade2ab2a10
JacNo <jcknowa@gmail.com>
2021-10-17 12:43:08 +0200

    showing memes by user

--- src/main/java/pl/sdacademy/projekt3/repositories/MemeRepository.java
+++ src/main/java/pl/sdacademy/projekt3/repositories/MemeRepository.java
@@ -3,5 +3,8 @@ package pl.sdacademy.projekt3.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.sdacademy.projekt3.entities.Meme;
 
+import java.util.List;
+
 public interface MemeRepository extends JpaRepository<Meme, Integer> {
+    List<Meme> findAllByUser(String user);
 }
