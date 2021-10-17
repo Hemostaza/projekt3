2765d21d673d4fe49ee9eba2ac1e8e1157e0ed0b
JacNo <jcknowa@gmail.com>
2021-10-17 09:36:05 +0200

    Functionality: register user in form, added getters and setter for user,

--- src/main/java/pl/sdacademy/projekt3/controllers/UserController.java
+++ src/main/java/pl/sdacademy/projekt3/controllers/UserController.java
@@ -3,7 +3,10 @@ package pl.sdacademy.projekt3.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pl.sdacademy.projekt3.entities.Meme;
 import pl.sdacademy.projekt3.entities.User;
 import pl.sdacademy.projekt3.repositories.UserRepository;
 
@@ -22,6 +25,15 @@ public class UserController {
     public String getList(ModelMap modelMap) {
         List<User> users = userRepository.findAll();
         modelMap.addAttribute("users", users);
-        return "redirect:/user/user_list";
+        return "user/user_list";
+    }
+    @GetMapping("/register")
+    public String registerUserForm(@ModelAttribute("user") User user){
+        return "user/user_register";
+    }
+    @PostMapping("/register")
+    public String registerUser(User user){
+        userRepository.save(user);
+        return "redirect:/user/list";
     }
 }
--- src/main/java/pl/sdacademy/projekt3/entities/User.java
+++ src/main/java/pl/sdacademy/projekt3/entities/User.java
@@ -13,4 +13,36 @@ public class User {
     private String login;
     private String password;
     private String email;
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
